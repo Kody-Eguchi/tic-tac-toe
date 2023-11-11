005d942c188c70f52fb5a0bdb4ed61198a3ed9ac
Koji Eguchi <koji.eguchi@hotmail.com>
2023-11-11 18:45:54 -0500

    Add Board component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,18 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-square',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      square works!
-      {{rando}}
-    </p>
-  `,
-  styles: ``
+  template:`
+  <button nbButton *ngIf="!value">{{ value }}</button>
+  <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+  <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+`,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
-rando = Math.random();
+  @Input() value: string;
 }
